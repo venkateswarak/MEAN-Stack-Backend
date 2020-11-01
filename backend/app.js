f47fe620b61940343f714c0685f91730fc81618c
Venkateswara Kotha <venkateswarakotha@Venkateswaras-MacBook-Pro.local>
2020-11-01 14:15:26 -0500

    added routes

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
 const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://venkatkotha:92V2vaQ93bPVNVW@cluster0.yznje.mongodb.net/angular-nodeApp?retryWrites=true&w=majority')
-.then(() => console.log('Connected to Server'))
-.catch(() => console.log('Connection Failed!'));
+mongoose
+  .connect(
+    "mongodb+srv://venkatkotha:92V2vaQ93bPVNVW@cluster0.yznje.mongodb.net/angular-nodeApp?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,42 +28,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-//MongoKey//92V2vaQ93bPVNVW
-
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  })
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: result._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-Post.find().then(documents =>
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: documents
-  }))
-
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(() =>
-    res.status(200).json({
-      message: 'deleted post successfully!'
-    })
-  )
-
-  });
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
